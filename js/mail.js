06d8eabbeb51164d895931e5e661e11e6120ffbb
shchurinov <yshchurinov@gmail.com>
2026-01-15 11:30:03 +0300

    updated scripts

--- js/mail.js
+++ js/mail.js
@@ -1,4 +1,4 @@
-var em1 = "yshchurinov"
+var em1 = "shchurinov"
 var em2 = "@"
-var em3 = "outlook.com"
+var em3 = "icloud.com"
 document.write('<a href=\"mailto:' + em1 + em2 + em3 +'\">' + em1 + em2 + em3 + '</a>');
