82c455f91fc21aee1197126df9ed5f063885af37
shchurinov <yshchurinov@gmail.com>
2026-05-12 14:44:03 +0300

    updated scripts for copying

--- js/button.js
+++ js/button.js
@@ -9,3 +9,25 @@ function myFunction() {
 		// Copy the text inside the text field
 		navigator.clipboard.writeText(copyText.value);
 			} 
+function copyBTC() {
+		// Get the text field
+		var copyText = document.getElementById("btc");
+
+		// Select the text field
+		copyText.select();
+		copyText.setSelectionRange(0, 99999); // For mobile devices
+
+		// Copy the text inside the text field
+		navigator.clipboard.writeText(copyText.value);
+			} 
+function copyDoge() {
+		// Get the text field
+		var copyText = document.getElementById("doge");
+
+		// Select the text field
+		copyText.select();
+		copyText.setSelectionRange(0, 99999); // For mobile devices
+
+		// Copy the text inside the text field
+		navigator.clipboard.writeText(copyText.value);
+			} 
